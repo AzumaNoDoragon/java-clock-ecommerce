96b309d93f5d824279a688f216b9d27691ea7370
Gabriel Afini <gabrielsads@gmail.com>
2025-07-15 12:15:58 -0300

    Feat: alterado para o cors permitir apenas meu http e apenas GET

--- src/main/java/com/dio/dio/clocks/Config/WebConfig.java
+++ src/main/java/com/dio/dio/clocks/Config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET");
     }
 }
