c32aebd79239eeafc5302bf59c4ef294211f6575
Gabriel Afini <gabrielsads@gmail.com>
2025-07-15 12:14:29 -0300

    Refact: Removido variavel não utilizada

--- src/main/java/com/dio/dio/clocks/Services/ProductServices.java
+++ src/main/java/com/dio/dio/clocks/Services/ProductServices.java
@@ -2,7 +2,6 @@ package com.dio.dio.clocks.Services;
 
 import com.dio.dio.clocks.Data.ClockData;
 import com.dio.dio.clocks.Domain.Clock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +9,6 @@ import java.util.Optional;
 
 @Service
 public class ProductServices {
-
-    @Autowired
-    private ClockData data;
-
     public List<Clock> getAllProducts() {
         return ClockData.getClocksDatabase();
     }
